a84b8c2ddacfcc084eeea7abd961bae037dc40b7
정유석 <j961224@naver.com>
2020-11-11 21:14:56 +0900

    Baekjoon Coding

---
+++ 11055/BJ11055.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class BJ11055{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] dp = new int[n];
+        
+        for(int i=0; i<n; i++){
+            a[i]=sc.nextInt();
+        }
+        
+        int max=0;
+        for(int i=0; i<n; i++){
+            dp[i]=a[i];
+            for(int j=0; j<i; j++){
+                if(a[j]<a[i] && dp[i]<a[i]+dp[j]){
+                    dp[i]=a[i]+dp[j];
+                }
+            }
+            max=Math.max(max,dp[i]);
+        }
+        System.out.println(max);
+        
+    }
+}
---
+++ 1912/BJ1912.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+// 처음에 dfs로 했으나 시간 초과! -> 연속된 숫자이니 DP에 LIS를 유사하게 생각
+
+public class BJ1912{
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        int[] a = new int[cnt];
+        int[] dp = new int[cnt];
+        
+        for(int i=0; i<cnt; i++){
+            a[i]=sc.nextInt();
+        }
+        
+        int max = a[0];
+        dp[0]=a[0];
+        for(int j=1; j<a.length; j++){
+            dp[j]=Math.max(dp[j-1]+a[j],a[j]);
+            
+            max=Math.max(dp[j],max);
+        }
+        
+        System.out.println(max);
+    }
+    
+
+}
