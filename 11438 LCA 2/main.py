4a1aedb81c2cdbbed40c92b9010f6c557e8dd9cc
정유석 <j961224@naver.com>
2021-07-12 17:46:57 +0900

    Baekjoon Algorithm Source code(graph)(SDS 알고리즘 특강)

---
+++ 11438 LCA 2/main.py
@@ -0,0 +1,55 @@
+import sys
+sys.setrecursionlimit(100000)
+input = sys.stdin.readline
+Length = 21
+
+n = int(input())
+parent = [[0]*Length for _ in range(n+1)]
+visited = [False]*(n+1)
+d = [0]*(n+1)
+graph = [[] for _ in range(n+1)]
+
+for _ in range(n-1):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(x,depth):
+    visited[x]=True
+    d[x]=depth
+    
+    for node in graph[x]:
+        if(visited[node]):
+            continue
+        parent[node][0]=x
+        dfs(node,depth+1)
+
+def set_parent():
+    dfs(1,0)
+    for i in range(1,Length):
+        for j in range(1,n+1):
+            parent[j][i]=parent[parent[j][i-1]][i-1]
+
+def lca(a,b):
+    if d[a]>d[b]:
+        a,b = b,a
+    
+    for i in range(Length-1,-1,-1):
+        if d[b]-d[a]>=2**i:
+            b = parent[b][i]
+    if a==b:
+        return a
+    
+    for i in range(Length-1,-1,-1):
+        if parent[a][i] != parent[b][i]:
+            a = parent[a][i]
+            b = parent[b][i]
+    return parent[a][0]
+
+set_parent()
+
+m = int(input())
+
+for _ in range(m):
+    a,b = map(int,input().split())
+    print(lca(a,b))
